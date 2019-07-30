ffa223857f861b838ba2854f097d6785d2a1b098
sshleifer <sshleifer@gmail.com>
2019-07-30 08:26:26 -0700

    More clargs

--- train.py
+++ train.py
@@ -195,7 +195,7 @@ def train(args):
     tokenizer = tokenizer_class.from_pretrained(args.model_checkpoint)
     model_class = GPT2DoubleHeadsModel if "gpt2" in args.model_checkpoint else OpenAIGPTDoubleHeadsModel
     model = model_class.from_pretrained(args.model_checkpoint)
-    if getattr(args, 'ckpt_path'):
+    if args.ckpt_path:
         state_dict = torch.load(args.ckpt_path)
         extra_toks = state_dict['transformer.tokens_embed.weight'].shape[0] - model.transformer.tokens_embed.weight.shape[0]
         model.set_num_special_tokens(extra_toks)
@@ -216,7 +216,7 @@ def train(args):
 
     logger.info("Prepare datasets")
     personachat = pickle_load(args.dataset_path)
-    #personachat = get_dataset(tokenizer, args.dataset_path, args.dataset_cache) # TODO(SS): make this
+    #personachat = get_dataset(tokenizer, args.dataset_path, args.dataset_cache)
     train_loader, val_loader, train_sampler, valid_sampler = get_data_loaders(personachat, args, tokenizer)
 
     # Training function and trainer
@@ -323,30 +323,41 @@ def train(args):
 
 
 
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
 
-    # parser.add_argument("--dataset_path", type=str, default="", help="Path or url of the dataset. If empty download from S3.")
-    # parser.add_argument("--dataset_cache", type=str, default='./dataset_cache', help="Path or url of the dataset cache")
-    # parser.add_argument("--model_checkpoint", type=str, default="openai-gpt", help="Path, url or short name of the model")
-    # parser.add_argument("--num_candidates", type=int, default=2, help="Number of candidates for training")
-    # parser.add_argument("--max_history", type=int, default=2, help="Number of previous exchanges to keep in history")
-    # parser.add_argument("--train_batch_size", type=int, default=4, help="Batch size for training")
-    # parser.add_argument("--valid_batch_size", type=int, default=4, help="Batch size for validation")
-    # parser.add_argument("--gradient_accumulation_steps", type=int, default=8, help="Accumulate gradients on several steps")
-    # parser.add_argument("--lr", type=float, default=6.25e-5, help="Learning rate")
-    # parser.add_argument("--lm_coef", type=float, default=1.0, help="LM loss coefficient")
-    # parser.add_argument("--mc_coef", type=float, default=1.0, help="Multiple-choice loss coefficient")
-    # parser.add_argument("--max_norm", type=float, default=1.0, help="Clipping gradient norm")
-    # parser.add_argument("--n_epochs", type=int, default=3, help="Number of training epochs")
-    # parser.add_argument("--personality_permutations", type=int, default=1, help="Number of permutations of personality sentences")
-    # parser.add_argument("--eval_before_start", action='store_true', help="If true start with a first evaluation before training")
-    # parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Device (cuda or cpu)")
-    # parser.add_argument("--fp16", type=str, default="", help="Set to O0, O1, O2 or O3 for fp16 training (see apex documentation)")
+    parser.add_argument("--dataset_path", type=str, default="tokenized_and_split_v2.pkl", help="Path or url of the dataset. If empty download from S3.")
+    parser.add_argument("--dataset_cache", type=str, default='./dataset_cache', help="Path or url of the dataset cache")
+    parser.add_argument("--model_checkpoint", type=str, default="openai-gpt", help="Path, url or short name of the model")
+    parser.add_argument("--num_candidates", type=int, default=2, help="Number of candidates for training")
+    parser.add_argument("--max_history", type=int, default=2, help="Number of previous exchanges to keep in history")
+    parser.add_argument("--train_batch_size", type=int, default=4, help="Batch size for training")
+    parser.add_argument("--valid_batch_size", type=int, default=4, help="Batch size for validation")
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=8, help="Accumulate gradients on several steps")
+    parser.add_argument("--lr", type=float, default=6.25e-5, help="Learning rate")
+    parser.add_argument("--lm_coef", type=float, default=1.0, help="LM loss coefficient")
+    parser.add_argument("--mc_coef", type=float, default=1.0, help="Multiple-choice loss coefficient")
+    parser.add_argument("--max_norm", type=float, default=1.0, help="Clipping gradient norm")
+    parser.add_argument("--n_epochs", type=int, default=3, help="Number of training epochs")
+    parser.add_argument("--personality_permutations", type=int, default=1, help="Number of permutations of personality sentences")
+    parser.add_argument("--eval_before_start", action='store_true', help="If true start with a first evaluation before training")
+    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Device (cuda or cpu)")
+    parser.add_argument("--fp16", type=str, default="", help="Set to O0, O1, O2 or O3 for fp16 training (see apex documentation)")
     parser.add_argument("--local_rank", type=int, default=-1, help="Local rank for distributed training (-1: not distributed)")
-    parser.add_argument("--args_path", type=str, help='path to args.pkl')
-    base_args = parser.parse_args()
+    parser.add_argument("--ignore_persona", action='store_true')
 
-    args = pickle_load(base_args.args_path)
-    args.local_rank = base_args.local_rank
+    # Additions
+    parser.add_argument('save_dir', type=str)
+    parser.add_argument('--ckpt_path', type=str, default='')
+    parser.add_argument('--eval_every', default=None)
+
+
+    args = parser.parse_args()
+
+    #import ipdb; ipdb.set_trace()
+
+    #args = pickle_load(base_args.args_path)
+    #args.local_rank = base_args.local_rank
     train(args)
