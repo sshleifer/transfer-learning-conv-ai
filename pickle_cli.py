70dd632f9937b5f93e1bd8433d1060f3f5a1b799
sshleifer <sshleifer@gmail.com>
2019-07-30 08:32:44 -0700

    attempt at pickle_cli

---
+++ pickle_cli.py
@@ -0,0 +1,10 @@
+from train import train
+from durbango import *
+import fire
+
+def train_args(args_path,, local_rank=-1):
+    args = pickle_load(args_path)
+    args.local_rank = local_rank
+    train(args)
+
+if __name__ == '__main__': fire.Fire(train_args)
